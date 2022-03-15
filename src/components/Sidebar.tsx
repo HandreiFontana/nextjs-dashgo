99b1f852a8ada523b47e042b599af079cae0a868
Handrei Fontana <95292655+HandreiFontana@users.noreply.github.com>
2022-03-15 13:56:29 -0300

    feat(dashboard): dashboard updated

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -12,11 +12,11 @@ export function Sidebar() {
                         fontSize="small"
                     >GERAL</Text>
                     <Stack spacing="4" mt="8" align="stretch">
-                        <Link display="flex">
+                        <Link display="flex" alignItems="center">
                             <Icon as={RiDashboardLine} fontSize="20" />
                             <Text ml="4" fontWeight="medium">Dashboard</Text>
                         </Link>
-                        <Link display="flex">
+                        <Link display="flex" alignItems="center">
                             <Icon as={RiContactsLine} fontSize="20" />
                             <Text ml="4" fontWeight="medium">usuários</Text>
                         </Link>
@@ -29,11 +29,11 @@ export function Sidebar() {
                         fontSize="small"
                     >AUTOMAÇÃO</Text>
                     <Stack spacing="4" mt="8" align="stretch">
-                        <Link display="flex">
+                        <Link display="flex" alignItems="center">
                             <Icon as={RiInputMethodLine} fontSize="20" />
                             <Text ml="4" fontWeight="medium">Formulários</Text>
                         </Link>
-                        <Link display="flex">
+                        <Link display="flex" alignItems="center">
                             <Icon as={RiGitMergeLine} fontSize="20" />
                             <Text ml="4" fontWeight="medium">Automação</Text>
                         </Link>
