e625b48d5efe53328230374fa27f45941cf47062
Handrei Fontana <95292655+HandreiFontana@users.noreply.github.com>
2022-03-15 12:03:14 -0300

    feat(header): header finished but needs to be perf

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
-import { Flex, Input, Text, Icon } from '@chakra-ui/react';
-import { RiSearchLine } from 'react-icons/ri'
+import { Flex, Input, Text, Icon, HStack, Box, Avatar } from '@chakra-ui/react';
+import { RiNotificationLine, RiSearchLine, RiUserAddLine } from 'react-icons/ri'
 
 export function Header() {
     return (
@@ -50,6 +50,43 @@ export function Header() {
                 ></Input>
 
                 <Icon as={RiSearchLine} fontSize="20" />
+            </Flex>
+
+            <Flex
+                align="center"
+                ml="auto"
+            >
+                <HStack
+                    spacing="8"
+                    mx="8"
+                    pr="8"
+                    py="1"
+                    color="gray.300"
+                    borderRightWidth="1px"
+                    borderColor="gray.700"
+                >
+                    <Icon as={RiNotificationLine} fontSize="20" />
+                    <Icon as={RiUserAddLine} fontSize="20" />
+                </HStack>
+
+                <Flex align="center">
+                    <Box
+                        mr="4"
+                        textAlign="right"
+                    >
+                        <Text>Handrei Fontana</Text>
+                        <Text
+                            color="gray.300"
+                            fontSize="small"
+                        >handreifontana@gmail.com</Text>
+                    </Box>
+
+                    <Avatar
+                        size="md"
+                        name="Handrei Fontana"
+                        src="https://github.com/handreifontana.png"
+                    />
+                </Flex>
             </Flex>
         </Flex>
 
