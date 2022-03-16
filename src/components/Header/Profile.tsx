f170c195f7f0ed36daad6db4c1e8b7c45aab297a
Handrei Fontana <95292655+HandreiFontana@users.noreply.github.com>
2022-03-16 10:27:54 -0300

    perf(responsivity): header responses by wide width

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,18 +1,24 @@
 import { Avatar, Box, Flex, Text } from "@chakra-ui/react";
 
-export function Profile() {
+interface ProfileProps {
+    showProfileData?: boolean;
+}
+
+export function Profile({ showProfileData = true }: ProfileProps) {
     return (
         <Flex align="center">
-            <Box
-                mr="4"
-                textAlign="right"
-            >
-                <Text>Handrei Fontana</Text>
-                <Text
-                    color="gray.300"
-                    fontSize="small"
-                >handreifontana@gmail.com</Text>
-            </Box>
+            {showProfileData && (
+                <Box
+                    mr="4"
+                    textAlign="right"
+                >
+                    <Text>Handrei Fontana</Text>
+                    <Text
+                        color="gray.300"
+                        fontSize="small"
+                    >handreifontana@gmail.com</Text>
+                </Box>
+            )}
 
             <Avatar
                 size="md"
