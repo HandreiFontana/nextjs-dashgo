4756f6de27f27b01c20d890b763ff245a2d37a8f
Handrei Fontana <95292655+HandreiFontana@users.noreply.github.com>
2022-03-16 12:56:13 -0300

    perf(style): active link in navsection now has a new color

--- src/components/Sidebar/SidebarNav.tsx
+++ src/components/Sidebar/SidebarNav.tsx
@@ -9,7 +9,7 @@ export function SidebarNav() {
         <Stack spacing="12" align="flex-start">
             <NavSection title="GERAL">
                 <NavLink icon={RiDashboardLine} href="/dashboard">Dashboard</NavLink>
-                <NavLink icon={RiContactsLine} href="/users">usuários</NavLink>
+                <NavLink icon={RiContactsLine} href="/users">Usuários</NavLink>
             </NavSection>
             <NavSection title="AUTOMAÇÃO">
                 <NavLink icon={RiInputMethodLine} href="/forms">Formulários</NavLink>
