0c9a684405f9f573fea4f12dbd6dac7408b14535
Handrei Fontana <95292655+HandreiFontana@users.noreply.github.com>
2022-03-16 13:50:31 -0300

    feat(forms): react hook forms installed and sign in edited

--- src/components/Header/SearchBox.tsx
+++ src/components/Header/SearchBox.tsx
@@ -1,8 +1,16 @@
 import { Flex, Icon, Input } from "@chakra-ui/react";
+import { useRef, useState } from "react";
 import { RiSearchLine } from "react-icons/ri";
 
 
+
 export function SearchBox() {
+    // const [search, setSearch] = useState('')
+
+    const searchInputRef = useRef<HTMLInputElement>(null)
+
+    console.log(searchInputRef.current.value)
+
     return (
         <Flex
             as="label"
@@ -24,6 +32,7 @@ export function SearchBox() {
                 mr="4"
                 placeholder="Buscar na plataforma"
                 _placeholder={{ color: 'gray.400' }}
+                ref={searchInputRef}
             ></Input>
 
             <Icon as={RiSearchLine} fontSize="20" />
